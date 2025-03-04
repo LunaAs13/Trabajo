a3ad5fd18429612f8b63ded6051985a14590870f
dap00k <daniel.andres.perez@alumnos.upm.es>
2025-03-04 10:25:14 +0100

    Update prueba.sh

--- prueba.sh
+++ prueba.sh
@@ -4,63 +4,22 @@
 
 palabra1=$1
 numeroColumna=-1
+CSV_NOM=$2
 
 #Buenos dias 
 
 function numeroColumna(){
-	nombre=$1
-	case "$nombre" in
-		"rownames")
-			numeroColumna=1
-			;;
-		"idNum")
-			numeroColumna=2
-			;;
-		"date")
-			numeroColumna=3
-			;;
-		"problem")
-			numeroColumna=4
-			;;
-		"MDC")
-			numeroColumna=5
-			;;
-		"citationIssued")
-			numeroColumna=6
-			;;
-		"personSearch")
-			numeroColumna=7
-			;;
-		"vehicleSearch")
-			numeroColumna=8
-			;;
-		"preRace")
-			numeroColumna=9
-			;;
-		"race")
-			numeroColumna=10
-			;;
-		"gender")
-			numeroColumna=11
-			;;
-		"lat")
-			numeroColumna=12
-			;;
-		"long")
-			numeroColumna=13
-			;;
-		"policePrecinct")
-			numeroColumna=14
-			;;
-		"neighborhood")
-			numeroColumna=15
-			;;
-		*)
-			echo "error no valid column"
-			exit 1
-			;;
-esac
-
+	NOM_COLUMNAS=$(cat $CSV_NOM | head -n1 | sed 's/,/ /g' | sed -E 's/^[^[:space:]]+[[:space:]]*//')
+	CNT=1
+ 	for NOMBRE in $NOM_COLUMNAS
+  	do
+   		if [ NOMBRE -eq $1 ]
+     		then
+     			numeroColumna=$CNT
+		else
+  			((CNT++))
+  		fi
+   	done
 }
 
 
@@ -68,13 +27,13 @@ if [[ "${palabra1,,}" == "-help" ]]; then
 	echo "The use is ./filtro.sh [filterName] [argument]"
 	echo "The filters implemented are:"
 	echo "-help"
- 	echo "-columnNames
+ 	echo "-columnNames [CSV FILE]
   		Show all valid column names"
-    	echo "-columnValues [columnName]
+    	echo "-columnValues [CSV FILE] [columnName]
      		Show all the different values that appear in the specified column"
-	echo "-filterColumn [columnName]
+	echo "-filterColumn [CSV FILE] [columnName]
 		Show the records of a column"
-	echo "-filterColumnValue [columnName] [nValues] [value1] [value2] ... [valueN] 
+	echo "-filterColumnValue [CSV FILE] [columnName] [nValues] [value1] [value2] ... [valueN] 
 		Show all complete records with the indicated values in the column"
 	echo "-filterDate [beginingDate] [endDate]
 		Shows all complete records between beginingDate and endDate if endDate is not 
@@ -82,7 +41,7 @@ if [[ "${palabra1,,}" == "-help" ]]; then
 	echo "-filterTime [beginingTime] [endTime]
 		Shows all complete records between beginingTime and endTime if endTime is not 
 		indicated, it will show every record since beginingTime until midnight."
-   	echo "-filterNColumnValues [nColunm] [column1] [value1] ... [columnN] [valueN]
+   	echo "-filterNColumnValues [CSV FILE] [nColunm] [column1] [value1] ... [columnN] [valueN]
 		Show all complete record that contains the n indicated values in the specified columns"
 
 elif [[ "${palabra1,,}" == "-columnnames" ]]
@@ -93,56 +52,58 @@ elif [[ "${palabra1,,}" == "-columnvalues" ]]
 	then
  	if [[ -z $2 ]]
   	then
-  		echo "Specify the column name"
+  		echo "Specify the CSV"
 		exit 1
+  	elif [[ -z $3 ]]
+   	then
+   		echo "Specify the column name"
+     		exit 1
     	break
-    fi
- 	nombreColumna=$2
+    	fi
+ 	nombreColumna=$3
   	numeroColumna $nombreColumna
  	cat MplsStops.csv | awk -F "," -v col="$numeroColumna" 'NR>1{print $col}' | sort -u
 
 elif [[ "${palabra1,,}" == "-filtercolumn" ]] 
 	then
-	if [[ -z $2 ]]
-	then
-		echo "Specify the column name"
+ 	if [[ -z $2 ]]
+  	then
+  		echo "Specify the CSV"
 		exit 1
-		break
-	fi
-	nombreColumna=$2
+  	elif [[ -z $3 ]]
+   	then
+   		echo "Specify the column name"
+     		exit 1
+    	break
+    	fi
+	nombreColumna=$3
 	numeroColumna $nombreColumna
 	cat MplsStops.csv  | awk -F "," -v col="$numeroColumna" 'NR>1{print $col}'
  
 elif [[ "${palabra1,,}" == "-filtercolumnvalue" ]]
 	then
-	if [[ -z $2 || -z $3 || -z $4 ]]
+	if [[ -z $2 || -z $3 || -z $4 || -z $5 ]]
 	then
-		echo "The use of -filterColumnValue requires 3 parameters"
+		echo "The use of -filterColumnValue requires 4 parameters"
 		exit 1
 		break
 	fi
 
 	if [[ ! "$3" =~ ^[0-9]+$ ]]
 	then
-		echo "The second parameter introduced must be the number of values you introduced"
+		echo "The third parameter introduced must be the number of values you introduced"
 		
 	fi
  
-	nombreColumna=$2
+	nombreColumna=$3
 	numeroColumna $nombreColumna
-	numeroParametros=$3
+	numeroParametros=$4
 
  	CONT=1
   	while [ $CONT -le $numeroParametros ]
    	do
-		num=$((CONT+3))
+		num=$((CONT+4))
 		valor=${!num}
-
-		if [[ ! "$valor" =~ ^[a-zA-Z]+$ ]]
-		then
-			echo "The value of $valor is not a string"
-			exit 1
-		fi
 		
 		cat MplsStops.csv | awk -F "," -v nCol="$numeroColumna" -v bien="$valor" 'NR>1{
 				if ($nCol == bien) {
@@ -225,26 +186,23 @@ elif [[ "${palabra1,,}" == "-filtertime" ]]
 elif [[ "${palabra1,,}" == "-filterncolumnvalues" ]]
 	then
 		echo ""
-		if [[ -z $2 || -z $3 || -z $4 ]]
+		if [[ -z $2 || -z $3 || -z $4 || -z $5 ]]
 		then
-				echo "The use of -filterncolumnvalues requires at least 3 parameters"
-				exit 1
-				break
+			echo "The use of -filterNColumnValues requires at least 4 parameters"
+			exit 1
 		fi
-		if [[ ! "$2" =~ ^[0-9]+$ ]]
-	then
-		echo "The first parameter introduced must be the number of values you introduced"
+		if [[ ! "$3" =~ ^[0-9]+$ ]]
+		then
+			echo "The third parameter introduced must be the number of values you introduced"
 		exit 1
 	fi
 		
-	num=$2
-	PUNTERO=3
+	num=$3
+	PUNTERO=4
 	IT=0
-	#eliminar la linea de los nombres de la columna
 	cat MplsStops.csv | awk 'NR>1' > help.txt
 	while [ $IT -lt $num ]
 	do
-		#cat help.txt | wc -l
 		columna=${!PUNTERO}
 		if [[ ! "$columna" =~ ^[a-zA-Z]+$ ]]
 			then
@@ -255,12 +213,6 @@ elif [[ "${palabra1,,}" == "-filterncolumnvalues" ]]
 		numeroColumna $columna
 		((PUNTERO++))
 		valor=${!PUNTERO}
-		if [[ ! "$valor" =~ ^[a-zA-Z]+$ ]]
-			then
-				echo "The value of $valor is not a string"
-				exit 1
-		fi
-
 		((PUNTERO++))
 
 		cat help.txt | awk -F "," -v nCol="$numeroColumna" -v bien="$valor" '{
